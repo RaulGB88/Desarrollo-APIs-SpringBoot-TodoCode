415bc01ce757ae7bbb2d49cdb9e60af36b979919
RaulGB88 <rgranel@viewnext.com>
2023-11-21 18:10:08 +0100

    v0.1.0.2

--- 12-ResponseBody/cursoSpringBoot/src/main/java/com/todocodeacademy/cursoSpringBoot/controller/apliacionController.java
+++ 12-ResponseBody/cursoSpringBoot/src/main/java/com/todocodeacademy/cursoSpringBoot/controller/apliacionController.java
@@ -1,7 +1,10 @@
 package com.todocodeacademy.cursoSpringBoot.controller;
+
 import com.todocodeacademy.cursoSpringBoot.Cliente;
+
 import java.util.ArrayList;
 import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,25 +13,24 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class apliacionController {
-  
-    @GetMapping ("/cliente/traer")
+
+    @GetMapping("/cliente/traer")
     @ResponseBody
-    public List<Cliente> traerClientes () {
-    
+    public List<Cliente> traerClientes() {
+
         List<Cliente> listaClientes = new ArrayList<Cliente>();
         listaClientes.add(new Cliente(1L, "Zlatan", "Ibrahimovic"));
         listaClientes.add(new Cliente(2L, "Cristiano", "Ronaldo"));
         listaClientes.add(new Cliente(3L, "Lionel", "Messi"));
-        
+
         return listaClientes;
-        
+
     }
-    
-    @GetMapping ("/pruebaresponse")
+
+    @GetMapping("/pruebaresponse")
     ResponseEntity<String> traerRespuesta() {
-        return new ResponseEntity<> ("Esta es una prueba de response", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>("Esta es una prueba de response", HttpStatus.NOT_FOUND);
     }
- 
-   
+
 }
 
